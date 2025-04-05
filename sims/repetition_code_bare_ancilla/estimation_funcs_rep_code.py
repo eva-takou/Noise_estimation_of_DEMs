69f535ae727c1af11907105a475e7c96eb38e815
Eva Takou <etakou@vt.edu>
2025-04-05 11:02:09 -0400

    cleanup

--- sims/repetition_code_bare_ancilla/estimation_funcs_rep_code.py
+++ sims/repetition_code_bare_ancilla/estimation_funcs_rep_code.py
@@ -13,17 +13,14 @@ def estimate_time_edge_probs(num_rounds:int, num_ancilla:int, vi_mean, vivj_mean
     These are all bulk edges because one error creates two detector events.
     We only evaluate the error probabilities of consecutive time-edges.
 
-    Input
-    -------
-    num_rounds: # of QEC rounds
-    num_ancilla: # of ancilla qubits
-    vi_mean: # of times each detector fires / # of shots (dims: # of qec rounds x # of ancilla )
-    vi_vj_mean: # of times 2 detectors fire together (correlations) / # of shots (dims: # of qec rounds  x # of qec rounds x # of ancilla x # of ancilla)
+    Input:
+        num_rounds: # of QEC rounds
+        num_ancilla: # of ancilla qubits
+        vi_mean: # of times each detector fires / # of shots (dims: # of qec rounds x # of ancilla )
+        vi_vj_mean: # of times 2 detectors fire together (correlations) / # of shots (dims: # of qec rounds  x # of qec rounds x # of ancilla x # of ancilla)
 
-    Output
-    --------
-    Dictionary with time_edge_names as a field, and time-edge probabilities as a value
-    The edge name has the form ((round1,round2),(ancilla1,ancilla2)). For time-edges ancilla1==ancilla2
+    Output:
+        time_edge_probs: dictionary with keys the names of detectors of time edges, and values the estimated probabilities.
 
     '''
 
@@ -44,7 +41,6 @@ def estimate_time_edge_probs(num_rounds:int, num_ancilla:int, vi_mean, vivj_mean
             indx1 = l+num_ancilla*k1
             indx2 = l+num_ancilla*k2
 
-            # time_edge_probs[((k1,k2),(l,l))] = val #((ROUND1,ROUND2),(ANC,ANC))
             time_edge_probs[("D"+str(indx1),"D"+str(indx2))]=val
 
     return time_edge_probs  
@@ -59,17 +55,13 @@ def estimate_data_edge_bulk_probs_all(num_rounds: int, num_ancilla: int, vi_mean
     Get the estimated probability for any 2 points in the bulk of the code 
     (we do not include potential boundary edges, but include all other space-time edges).
 
-    Input
-    ------
-    num_rounds: # of QEC rounds
-    num_ancilla: # of ancilla qubits
-    vi_mean: # of times each detector fires / # of shots (dims: # of qec rounds x # of ancilla )
-    vi_vj_mean: # of times 2 detectors fire together (correlations) / # of shots (dims: # of qec rounds  x # of qec rounds x # of ancilla x # of ancilla)
-    dictionary_format: True or False to output the probabilities as dictionary or not. 
-                       If the dictionary_format = False, then output an array of dims (num_rounds x num_ancilla) x (num_rounds x num_ancilla) 
-                       where the row index is indx1 = rd1 + num_rounds*l1 
-                       and the column index is indx2 = rd2 + num_rounds*l2 
-                       with l1,l2 \in [0,num_ancilla-1] and rd1,rd2 \in [0,num_rounds]
+    Input:
+        num_rounds: # of QEC rounds
+        num_ancilla: # of ancilla qubits
+        vi_mean: # of times each detector fires / # of shots (dims: # of qec rounds x # of ancilla )
+        vi_vj_mean: # of times 2 detectors fire together (correlations) / # of shots (dims: # of qec rounds  x # of qec rounds x # of ancilla x # of ancilla)
+    Output:
+        pij_bulk: dictionary with keys the bulk detectors and values the estimated probabilities.
 
     '''
     num_rounds += 1 #Because we add to the last QEC round the measurement of data qubits as another "QEC round"
@@ -112,32 +104,20 @@ def estimate_data_edge_bulk_probs_all(num_rounds: int, num_ancilla: int, vi_mean
 #----------------- Estimation of error probabilities for boundary edges --------------------------------
 
 
-
-def estimate_data_edge_bd_probs(num_rounds: int, num_ancilla: int, vi_mean: float, vivj_mean: float, 
-                                include_spacetime_edges: bool, dictionary_format: bool):
+def estimate_data_edge_bd_probs(num_rounds: int, num_ancilla: int, vi_mean: float, vivj_mean: float):
 
     '''
     Estimate the boundary edge probabilities corresponding to data qubits. For the repetition code
     the boundary edge errors are detected by the first ancilla qubit and the last ancilla qubit.
 
-    Input
-    -------
-    num_rounds:  # of qec rounds
-    num_ancilla: # of ancilla qubits
-    vi_mean:     # of times each detector fires/ # of shots (dims: # of rounds x # of ancilla)
-    vivj_mean:   # of times 2 detectors fire/ # of shots (dims: # of rounds x # of rounds x # of ancilla x # of ancilla)
-    include_spacetime_edges: True or False to include diagonal edges
+    Input:
+        num_rounds:  # of qec rounds
+        num_ancilla: # of ancilla qubits
+        vi_mean:     # of times each detector fires/ # of shots (dims: # of rounds x # of ancilla)
+        vivj_mean:   # of times 2 detectors fire/ # of shots (dims: # of rounds x # of rounds x # of ancilla x # of ancilla)
 
-    Output
-    -------
-    bd_probs: error probabilities of boundary edge data qubits (array of length # of rounds x # of "edge" ancilla)
-    bd_edge_names: names of the boundary edges for each error probability (list of tuples of the form (round,ancilla))
-                   (round k, ancilla 0) is the leftmost data edge for the k-th qec round.
-    
-    
-    For the diagonal edges that we include: they have to be adjacent in the following sense:
-    -If we consider the left bd point (s,t) then we have to take a diagonal edge (s+1,t-1) to form the diagonal edge
-    -If we consider the right bd point (s,t) then we have to take a diagonal edge (s-1,t+1) to form the diagonal edge
+    Output:
+        pij_bd: dictionary of boundary node names with values the estimated probabilities.
 
     '''
     num_rounds    = num_rounds+1 #Because we add to the last QEC round the measurement of data qubits as another QEC round
