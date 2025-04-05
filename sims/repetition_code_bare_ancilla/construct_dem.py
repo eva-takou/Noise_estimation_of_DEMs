2f624d69b20f1a3f23b527297d31ddefeeb10e8d
Eva Takou <etakou@vt.edu>
2025-04-05 11:15:16 -0400

    cleanup

--- sims/repetition_code_bare_ancilla/construct_dem.py
+++ sims/repetition_code_bare_ancilla/construct_dem.py
@@ -10,19 +10,17 @@ def get_measurement_data(samples,DATA_QUBITS: list,ANC_QUBITS: list,NUM_ROUNDS: 
     '''
     Extract the measurement outcomes as ancilla and data qubit outcomes, given the samples (from sampler) produced by stim.
 
-    Input
-    ------
-    samples: an array of the total samples collected from sampler.samples of stim (corresponding to data and ancilla qubits)
-    DATA_QUBITS: a list of str containing the data qubit names
-    ANC_QUBITS: a list of str containing the ancilla qubit names
-    NUM_ROUNDS: the total # of QEC rounds
-    NUM_SHOTS: the total # of shots
+    Input:
+        samples: an array of the total samples collected from sampler.samples of stim (corresponding to data and ancilla qubits)
+        DATA_QUBITS: a list of str containing the data qubit names
+        ANC_QUBITS: a list of str containing the ancilla qubit names
+        NUM_ROUNDS: the total # of QEC rounds
+        NUM_SHOTS: the total # of shots
 
 
-    Output
-    ------
-    anc_qubit_samples:  the ancilla qubit outcomes per shot and per QEC round. (xArray of size # of shots x # of rounds  x # of ancilla)
-    data_qubit_samples: the data qubit outcomes (last measurements). (xArray of size # shots x # of data qubits)
+    Output:
+        anc_qubit_samples:  the ancilla qubit outcomes per shot and per QEC round. (xArray of size # of shots x # of rounds  x # of ancilla)
+        data_qubit_samples: the data qubit outcomes (last measurements). (xArray of size # shots x # of data qubits)
 
     '''
     NUM_ANC            = len(ANC_QUBITS)
@@ -49,8 +47,14 @@ def get_measurement_data(samples,DATA_QUBITS: list,ANC_QUBITS: list,NUM_ROUNDS: 
     return anc_meas, data_meas 
 
 
-def get_initial_state(anc_qubits):
-
+def get_initial_state(anc_qubits: list):
+    '''Get the initial state for ancilla qubits. Assumed to be the all 0 state.
+    
+    Input: 
+        anc_qubits: list of integers denoting the ancilla qubit names
+    Output:
+        initial_state: xArray of all zeros, with dimensions the ancilla qubit names
+    '''
     #Initial ancilla state
     initial_state = np.zeros(len(anc_qubits), dtype=int)
     initial_state = initial_state==True
@@ -58,9 +62,20 @@ def get_initial_state(anc_qubits):
     
     return initial_state
 
-def project_data_meas(data_meas, num_shots,num_rounds,d,anc_qubits):
+def project_data_meas(data_meas,num_rounds: int,anc_qubits: list):
+    '''Perform stabilizer reconstruction of the last measurements on data qubit as another QEC round.
+    
+    Input: 
+        data_meas: xArray obtained from get_measurement_data, which corresponds to the data measurements
+        num_rounds: # of QEC rounds (int)
+        anc_qubits: list of integers corresponding to the ancilla qubit names
+    
+    Output:
+        syndrome_proj: xArray of dimensions # of shots x len(anc_qubits) corresponding to the syndrome projection onto stabilizers
+    '''
 
-    shots = np.arange(num_shots) 
+    num_shots = np.shape(data_meas)[0]
+    shots     = np.arange(num_shots) 
     
     syndrome_proj = data_meas.data[:,:-1] ^ data_meas.data[:,1:]  #dims: # of shots x # of ancilla
     #the 0th and 1st entries are xored, to give the 1st ancilla measurement equivalent
@@ -75,8 +90,19 @@ def project_data_meas(data_meas, num_shots,num_rounds,d,anc_qubits):
     return syndrome_proj
 
 
-def get_defects(num_shots, circuit,d, num_rounds):
-
+def get_defects(num_shots: int, circuit: stim.Circuit , d: int, num_rounds: int):
+    '''Reconstruct the defects matrix of the repetition code circuit.
+    
+    Input: 
+        num_shots: # of times to sample the raw measurements of the stim circuit (int)
+        circuit: the stim circuit (stim.Circuit)
+        d: distance of repetition code (int)
+        num_rounds: # of QEC rounds (int)
+    
+    Output:
+        defects_matrix: detector/defects matrix of dimesnsions # of shots x # of QEC rounds +1 x # of detectors (xArray)
+                       
+    '''
     data_qubits   = np.arange(0,d).tolist()
     anc_qubits    = np.arange(d,d+d-1).tolist()
 
@@ -87,7 +113,8 @@ def get_defects(num_shots, circuit,d, num_rounds):
 
     anc_meas,data_meas = get_measurement_data(samples,data_qubits,anc_qubits,num_rounds,num_shots)
 
-    syndrome_proj      = project_data_meas(data_meas,num_shots,num_rounds,d,anc_qubits)
+
+    syndrome_proj      = project_data_meas(data_meas,num_rounds,anc_qubits)
     syndromes          = xr.concat([anc_meas,syndrome_proj],"qec_round")
 
     syndrome_matrix_copy              = syndromes.copy()
@@ -98,9 +125,19 @@ def get_defects(num_shots, circuit,d, num_rounds):
     return defects_matrix 
 
 
-#Get the estimated error probabilities 
-def extract_error_probs(num_rounds,defects_matrix):
+def extract_error_probs(defects_matrix):
+    '''
+    Estimate all the probabilities of the detector error model of the repetition code.
 
+    Input: 
+        defects_matrix: detector/defects matrix of dimesnsions # of shots x # of QEC rounds +1 x # of detectors (xArray)
+    
+    Output:
+        pij_time: dictionary of the time edge error probabilities
+        pij_bd:   dictionary of the boundary edge error probabilities
+        pij_bulk: dictionary of the bulk edge error probabilitiies
+
+    '''
     num_rounds  = np.shape(defects_matrix)[1]-1
     num_ancilla = np.shape(defects_matrix)[2]
 
@@ -117,8 +154,15 @@ def extract_error_probs(num_rounds,defects_matrix):
     return pij_time,pij_bd,pij_bulk 
 
 
-def space_bulk_edge_names(num_rounds,num_ancilla):
-    '''Return the name of space edges in the bulk as a dictionary.'''
+def space_bulk_edge_names(num_rounds: int,num_ancilla: int):
+    '''Return the name of space edges in the bulk as a dictionary.
+    
+    Input:
+        num_rounds: # of QEC rounds (int)
+        num_ancilla: # of ancilla qubits (int)
+    Output:
+        bulk_edge_names: list of the bulk edge names with elements ("Dj","Dk")
+    '''
     
     bulk_edge_names = []
 
@@ -150,15 +194,27 @@ def space_bulk_edge_names(num_rounds,num_ancilla):
     return bulk_edge_names
 
 
-def construct_estimated_DEM(pij_time,pij_bd,pij_bulk,space_edge_names,stims_DEM):
+def construct_estimated_DEM(pij_time: dict, pij_bd: dict, pij_bulk: dict, space_edge_names: list, stims_DEM: stim.DetectorErrorModel):
+    '''Create the estimated detector error model. stims_DEM is only used to append the detector annotations.
+    
+    Input:
+        pij_time: dictionary of the time edge estimated probabilities
+        pij_bd:   dictionary of the boundary edge estimated probabilities
+        pij_bulk: dictionary of the bulk edge estimated probabilities
+        space_edge_names: list of the bulk edge name with elements of the form ("Dj","Dk")
+        stims_DEM: stim's noise-aware detector error model
 
-    my_DEM = stim.DetectorErrorModel()
+    Output:
+        reconstructed_DEM: the reconstructed detector error model
+
+    '''
+    reconstructed_DEM = stim.DetectorErrorModel()  
 
     for key in pij_bd.keys():
 
         det_id  = int(key[1:])
         targets = [stim.target_relative_detector_id(det_id),stim.target_logical_observable_id(0)]
-        my_DEM.append("error",pij_bd[key],targets)
+        reconstructed_DEM.append("error",pij_bd[key],targets)
     
     for key in pij_time.keys():
 
@@ -166,7 +222,7 @@ def construct_estimated_DEM(pij_time,pij_bd,pij_bulk,space_edge_names,stims_DEM)
         det_id1 = int(key[1][1:])
 
         targets = [stim.target_relative_detector_id(det_id0),stim.target_relative_detector_id(det_id1)]
-        my_DEM.append("error",pij_time[key],targets)
+        reconstructed_DEM.append("error",pij_time[key],targets)
 
     for name in space_edge_names:
 
@@ -175,7 +231,7 @@ def construct_estimated_DEM(pij_time,pij_bd,pij_bulk,space_edge_names,stims_DEM)
         targets = [stim.target_relative_detector_id(det_id0),stim.target_relative_detector_id(det_id1)]
         targets.append(stim.target_logical_observable_id(0))
         
-        my_DEM.append("error",pij_bulk[name],targets)
+        reconstructed_DEM.append("error",pij_bulk[name],targets)
 
 
     for instruction in stims_DEM:
@@ -183,8 +239,8 @@ def construct_estimated_DEM(pij_time,pij_bd,pij_bulk,space_edge_names,stims_DEM)
         if instruction.type=="error":
             continue
         else: #detector annotations
-            my_DEM.append(instruction)
+            reconstructed_DEM.append(instruction)
 
-    return my_DEM
+    return reconstructed_DEM
 
 
