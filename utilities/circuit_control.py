f68c540edce12c14857cc0b52b9da381c24cabfe
Eva Takou <etakou@vt.edu>
2025-04-25 10:51:36 -0400

    removed unused functions

--- utilities/circuit_control.py
+++ utilities/circuit_control.py
@@ -10,30 +10,3 @@ def get_str(circuit: stim.Circuit):
     '''
     return circuit.__str__()
 
-
-    '''General function to add bit-flip error before a measurement'''
-
-    str_circ = get_str(circuit)
-    new_str  = str_circ
-    L        = 2*len(new_str)
-
-    for k in range(L):
-
-        
-        if str_circ[k]=="M" and str_circ[k+1]=="R": #Measurement followed by reset
-
-            new_str = str_circ[:k+2] + """(""" + str(p_before_meas) + """)"""  + str_circ[k+2:]
-            
-
-        elif str_circ[k]=="M" and str_circ[k+1]!="X": #not the last MX gates on data qubits
-
-            new_str = str_circ[:k+1] + """(""" + str(p_before_meas) + """)"""  + str_circ[k+1:]
-            
-        
-        str_circ = new_str
-        if k==len(new_str)-1:
-            break
-        
-        
-
-    return stim.Circuit(new_str)
