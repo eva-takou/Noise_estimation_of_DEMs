b1ee35c70bfae1af925907a556685f5b087f1c09
Eva Takou <etakou@vt.edu>
2025-04-28 14:34:15 -0400

    minor update

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='Noise_Estimation_of_DEMs',
     version='0.1.0',    
     description='Estimating noise of detector error models',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/eva-takou/Noise_estimation_of_DEMs',
     author='Evangelia Takou',
     author_email='evangelia.takou@duke.edu',
